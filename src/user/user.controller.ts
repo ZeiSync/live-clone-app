30f39c8021fee9f19de535f061af03480b90665e
Pham Hoang <zekapako29@gmail.com>
2021-03-25 14:29:22 +0700

    update user profile and get user infor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Put,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
+import { GetUser } from './decorators/get-user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+import { UserService } from './user.service';
 
+@UseGuards(AuthGuard())
+@ApiTags('User')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async getUser(@GetUser() user: User): Promise<User> {
+    delete user['_doc'].password;
+    return user;
+  }
+
+  @Put()
+  @UsePipes(ValidationPipe)
+  async updateUser(
+    @GetUser() user: User,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    const userUpdated = await this.userService.update(user['_id'], {
+      ...user['_doc'],
+      ...updateUserDto,
+    });
+    delete userUpdated['_doc'].password;
+    return userUpdated;
+  }
+}
